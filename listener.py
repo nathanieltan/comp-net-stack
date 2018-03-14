f109a99ab95a10c9983ddad552420651e2e03513
Jeremy <jeremychristopherryan@gmail.com>
2018-03-13 22:38:08 -0400

    Added rough method outline for listener class

--- listener.py
+++ listener.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 
 class Listener():
-    __init__(self):
+    def __init__(self):
+        pass
+
+    def main(self):
+        """ Main loop that runs as long as object is listening. """
+        pass
+
+    def read_to_buffer(self):
+        """ Listens to GPIO and interprets bits that are sent on the line.
+        After end of message is detected, returns the message as a string. """
         pass
